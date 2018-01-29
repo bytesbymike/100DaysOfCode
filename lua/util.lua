fd1c3a328d993937d028cf54be4ba96ac5c5ef32
Mike Lamb <mrl@bytesbymike.com>
2018-01-29 16:08:23 -0500

    keys, pkeys - new functions

--- lua/util.lua
+++ lua/util.lua
@@ -1,4 +1,23 @@
 -- General utility functions
 
+-- Return sorted array of string keys
+local function util_keys(t)
+  local ks = {}
+  for k, _ in pairs(t) do
+    if type(k) == "string" then
+      table.insert(ks, k)
+    end
+  end
+  table.sort(ks)
+  return ks
+end
+
+local function util_pkeys(t)
+  local ks = util_keys(t)
+  for _, v in ipairs(ks) do print(v) end
+end
+
 return {
+  keys = util_keys,
+  pkeys = util_pkeys
 }
