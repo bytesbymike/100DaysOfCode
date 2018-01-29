73421f1d174c01ef08c994f9c00486d0570c6484
Mike Lamb <mrl@bytesbymike.com>
2018-01-29 15:03:47 -0500

    itr.foldl - new function

--- lua/itr.lua
+++ lua/itr.lua
@@ -1,6 +1,16 @@
 -- Stateful iterator that returns control, value.
 -- End of iteration is indicated by a control value of nil.
 
+local function itr_foldl(f, a, b)
+  local i, v = b()
+  if i == nil then
+    return a
+  else
+    return itr_foldl(f, f(a, v), b)
+  end
+end
+
 itr = {
+  foldl = itr
 }
 return itr
