46e6529afc6a8860f50c036f094d8f10bc1f325b
Mike Lamb <mrl@bytesbymike.com>
2018-01-29 15:02:17 -0500

    tn.itr - new function

--- lua/tn.lua
+++ lua/tn.lua
@@ -1,7 +1,20 @@
 -- Functions to operate on tables that consist of an array and "n"
 -- Tables may contain nil values.
 
+local function tn_itr(t)
+  local i = 0
+  return function ()
+    i = i + 1
+    if t.n < i then
+      return nil
+    else
+      return i, t[i]
+    end
+  end
+end
+
 tn = {
+  itr = tn_itr
 }
 return tn
 
