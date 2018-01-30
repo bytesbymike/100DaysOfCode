73191c23f0500b7f92afd5d5014db138f54e3df9
Mike Lamb <mrl@bytesbymike.com>
2018-01-29 23:02:57 -0500

    add missing comma

--- lua/tn.lua
+++ lua/tn.lua
@@ -22,9 +22,17 @@ local function tn_unpack(t)
   return table.unpack(t, 1, t.n)
 end
 
+local function tn_insert(t, itr)
+  
+end
+
+local function tn_cat(a, b)
+  
+end
+
 return {
   itr = tn_itr,
-  pack = tn_pack
+  pack = tn_pack,
   unpack = tn_unpack
 }
 
