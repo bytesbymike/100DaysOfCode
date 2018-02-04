83ee2f152b0b0c670ed5b52396e15d404e66fc23
Mike Lamb <mrl@bytesbymike.com>
2018-02-04 13:10:25 -0500

    remove functions without implementations

--- lua/tn.lua
+++ lua/tn.lua
@@ -7,14 +7,6 @@ local function tn_unpack(t)
   return table.unpack(t, 1, t.n)
 end
 
-local function tn_insert(t, itr)
-  
-end
-
-local function tn_cat(a, b)
-  
-end
-
 return {
   pack = tn_pack,
   unpack = tn_unpack
